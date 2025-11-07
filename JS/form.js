16a0178573544dbf5cd26dcae1678588fac97dc6
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2025-11-07 11:58:33 -0300

    Se agregaron los campos restantes al form

--- JS/form.js
+++ JS/form.js
@@ -5,6 +5,7 @@ form.addEventListener("submit", (event) => {
     const nombre = document.getElementById("nombre").value.trim();
     const email = document.getElementById("email").value.trim();
     const mensaje = document.getElementById("mensaje").value.trim();
+    
 
     if ( !nombre || !email|| !mensaje){
         alert("Completa todos los campos!");
